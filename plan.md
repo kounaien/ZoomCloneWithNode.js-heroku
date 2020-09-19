8d0143ff875e56f6e6065c769a8895cdb4a4fccb
kounaien <1127fujihiroto@gmail.com>
2020-09-20 01:45:22 +0900

    socket.io

--- plan.md
+++ plan.md
@@ -3,7 +3,7 @@
 - Initialize our NodeJS Project Done
 - Initialize our first view Done
 - Create a room id Done
-- Add the ability view our own Video
+- Add the ability view our own Video　Done
 - Add ability ro allow others to stream their video
 - Add styling 
 - Add the ability to create mesasges
--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+const socket = io('/')
 const videoGrid = document.getElementById('video-grid');
 console.log(videoGrid);
 const myVideo = document.createElement('video');
@@ -12,6 +13,8 @@ navigator.mediaDevices.getUserMedia({
     myVideoStream = stream;
     addVideoStream(myVideo, stream)
 });
+
+socket.emit('join-room');
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
